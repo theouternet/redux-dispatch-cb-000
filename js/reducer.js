ce2ca3f03ddd7e4ffbd9c1a44bf0edb3587e039f
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-06-28 20:49:34 +0000

    Automatically backed up by Learn

--- js/reducer.js
+++ js/reducer.js
@@ -1,13 +1,22 @@
-function changeState(state, action){
-  switch (action.type) {
-    case 'INCREASE_COUNT':
-      return {count: state.count + 1}
-    default:
-      return state;
-  }
-}
-
-let state = {count: 0}
-let action = {type: 'INCREASE_COUNT'}
-
-changeState(state, action)
+    let state = {count: 0};
+     
+    function changeState(state, action){
+        switch (action.type) {
+          case 'INCREASE_COUNT':
+            return {count: state.count + 1}
+          default:
+            return state;
+        }
+      }
+     
+    function dispatch(action){
+      state = changeState(state, action)
+      return state
+    }
+     
+    dispatch({type: 'INCREASE_COUNT'})
+      // => {count: 1}
+    dispatch({type: 'INCREASE_COUNT'})
+      // => {count: 2}
+    dispatch({type: 'INCREASE_COUNT'})
+      // => {count: 3}
