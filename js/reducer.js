727f9ab2ad83be6426d85e2d75c6419c37a1aecf
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-06-28 20:52:57 +0000

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,13 +1,19 @@
-function changeState(state, action){
-  switch (action.type) {
-    case 'INCREASE_COUNT':
-      return {count: state.count + 1}
-    default:
-      return state;
-  }
+    let state = {count: 0};
+     
+    function changeState(state, action){
+        switch (action.type) {
+          case 'INCREASE_COUNT':
+            return {count: state.count + 1}
+          default:
+            return state;
+        }
+      }
+     
+function dispatch(action){
+  state = changeState(state, action)
+  render()
 }
-
-let state = {count: 0}
-let action = {type: 'INCREASE_COUNT'}
-
-changeState(state, action)
+    
+function render(){
+  document.body.textContent = state.count
+}
